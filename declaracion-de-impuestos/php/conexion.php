ba7d585a5d540a4a10da1fd563f82c56d8eb2877
emanuel2414 <102926029+emanuel2414@users.noreply.github.com>
2022-04-15 13:24:34 -0500

    Add files via upload

--- declaracion-de-impuestos/php/conexion.php
+++ declaracion-de-impuestos/php/conexion.php
@@ -1,8 +1,8 @@
 <?php 
 	function conectarse(){
-		$equipo = "localhost";
-		$user = "root";
-		$pass = "";
+		$equipo = "iam0btyu0h0n.us-east-2.psdb.cloud";
+		$user = "ygj0ywko0idq";
+		$pass = "pscale_pw_JbAg-n3_FpePVFlDgb31FuatwoBiJGHFDy9AXFU93ww";
 		$bd = "declaracion_de_impuestos";
 
 		$conexion = mysqli_connect($equipo, $user, $pass, $bd);
