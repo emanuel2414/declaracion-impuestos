90bf12d9cece99550e944fd90333e184dbc962ce
emanuel2414 <102926029+emanuel2414@users.noreply.github.com>
2022-04-16 14:23:01 -0500

    Add files via upload

--- declaracion-de-impuestos/php/conexion.php
+++ declaracion-de-impuestos/php/conexion.php
@@ -1,9 +1,9 @@
 <?php 
 	function conectarse(){
-		$equipo = "iam0btyu0h0n.us-east-2.psdb.cloud";
-		$user = "ygj0ywko0idq";
-		$pass = "pscale_pw_JbAg-n3_FpePVFlDgb31FuatwoBiJGHFDy9AXFU93ww";
-		$bd = "declaracion_de_impuestos";
+		$equipo = "bmkrhyg9bmg0c1y3uzbj-mysql.services.clever-cloud.com";
+		$user = "uqdvesmp5f8wrfqw";
+		$pass = "vulGUZBJ64WujPJKmQF6";
+		$bd = "bmkrhyg9bmg0c1y3uzbj";
 
 		$conexion = mysqli_connect($equipo, $user, $pass, $bd);
 		return $conexion;
